d20c26ace5263befd7fe3f80bbd53b62d6cc98f5
Luke Tillman <luke.tillman@gmail.com>
2016-06-03 10:15:13 -0600

    Add powershell equivalent of build script

---
+++ build/docker-build.ps1
@@ -0,0 +1,16 @@
+<#
+    .DESCRIPTION
+    Runs a docker build for the DSE version specified in the DSE_VERSION file.
+#>
+
+$scriptPath = Split-Path -parent $PSCommandPath
+
+# Use contents of DSE_VERSION file as variables
+Get-Content "$scriptPath\DSE_VERSION" |% {
+    if ($_) {
+        $varParts = $_.Split('=')
+        New-Variable -Name $varParts[0] -Value $varParts[1]
+    }
+}
+
+docker build --build-arg DSE_VERSION=$DSE_VERSION -t luketillman/datastax-enterprise:$DSE_VERSION -t luketillman/datastax-enterprise:latest .
