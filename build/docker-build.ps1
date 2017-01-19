30ffbd3c1f2864fd39c977045f4ded621e9c074d
Luke Tillman <luke.tillman@gmail.com>
2017-01-19 09:28:52 -0700

    Build scripts should expect/use creds URL (#8)

--- build/docker-build.ps1
+++ build/docker-build.ps1
@@ -1,9 +1,15 @@
 <#
     .DESCRIPTION
-    Runs a docker build for the DSE version specified in the DSE_VERSION file.
+    Runs a docker build for the DSE version specified in the DSE_VERSION file. Expects the
+    DSE_CREDENTIALS_URL environment variable to be set.
 #>
 
 $scriptPath = Split-Path -parent $PSCommandPath
+
+# Make sure DSE_CREDENTIALS_URL is present
+if (!$Env:DSE_CREDENTIALS_URL) {
+    throw 'The DSE_CREDENTIALS_URL environment variable must be set before building'
+}
 
 # Use contents of DSE_VERSION file as variables
 Get-Content "$scriptPath\DSE_VERSION" |% {
@@ -13,4 +19,6 @@ Get-Content "$scriptPath\DSE_VERSION" |% {
     }
 }
 
-docker build --build-arg DSE_VERSION=$DSE_VERSION -t luketillman/datastax-enterprise:$DSE_VERSION .
+
+# Build the image
+docker build --build-arg DSE_VERSION=$DSE_VERSION --build-arg DSE_CREDENTIALS_URL=$Env:DSE_CREDENTIALS_URL -t luketillman/datastax-enterprise:$DSE_VERSION .
--- build/docker-build.sh
+++ build/docker-build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# Make sure we have the DSE_CREDENTIALS_URL available
+if [[ -z "$DOCKER_USER" ]]; then
+  echo 'The DSE_CREDENTIALS_URL environment variable must be set before building'
+  exit 1
+fi
+
 # Set the DSE_VERSION so it can be used then run docker build
 . "`dirname $0`/DSE_VERSION"
 
-docker build --build-arg DSE_VERSION=$DSE_VERSION -t luketillman/datastax-enterprise:$DSE_VERSION .
+docker build --build-arg DSE_VERSION=$DSE_VERSION --build-arg DSE_CREDENTIALS_URL=$DSE_CREDENTIALS_URL -t luketillman/datastax-enterprise:$DSE_VERSION .
